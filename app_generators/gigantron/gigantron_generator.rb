29d07665bbe3b4d35526abc2d735b836da041156
Ben Hughes <ben@pixelmachine.org>
2008-05-31 13:23:32 -0400

    Added application generator gigantron

---
+++ app_generators/gigantron/gigantron_generator.rb
@@ -0,0 +1,75 @@
+class GigantronGenerator < RubiGen::Base
+  
+  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
+                              Config::CONFIG['ruby_install_name'])
+  
+  default_options :author => nil
+  
+  attr_reader :name
+  
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @destination_root = File.expand_path(args.shift)
+    @name = base_name
+    extract_options
+  end
+
+  def manifest
+    record do |m|
+      # Ensure appropriate folder(s) exists
+      m.directory ''
+      BASEDIRS.each { |path| m.directory path }
+
+      # Create stubs
+       m.file "Rakefile", "Rakefile"
+       m.file "database.yml", "database.yml"
+
+       m.file "tasks/import.rake", "tasks/import.rake"
+
+       m.directory "test/models"
+       m.directory "test/tasks"
+       m.file "test/tasks/test_import.rb", "test/tasks/test_import.rb"
+      
+      m.dependency "install_rubigen_scripts", [destination_root, 'gigantron'], 
+        :shebang => options[:shebang], :collision => :force
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Creates a ...
+
+USAGE: #{spec.name} name
+EOS
+    end
+
+    def add_options!(opts)
+      opts.separator ''
+      opts.separator 'Options:'
+      # For each option below, place the default
+      # at the top of the file next to "default_options"
+      # opts.on("-a", "--author=\"Your Name\"", String,
+      #         "Some comment about this option",
+      #         "Default: none") { |options[:author]| }
+      opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+    end
+    
+    def extract_options
+      # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
+      # Templates can access these value via the attr_reader-generated methods, but not the
+      # raw instance variable value.
+      # @author = options[:author]
+    end
+
+    # Installation skeleton.  Intermediate directories are automatically
+    # created so don't sweat their absence here.
+    BASEDIRS = %w(
+      tasks
+      db
+      models
+      lib
+      test
+    )
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,4 @@
 require 'test/unit'
+require 'rubygems'
+require 'shoulda'
 require File.dirname(__FILE__) + '/../lib/gigantron'
