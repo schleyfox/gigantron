6be9d0734962ea499de5cd45636ef59c003a17cc
Ben Hughes <ben@pixelmachine.org>
2008-05-31 15:16:04 -0400

    Added model and task generators
    * basic stubs, seeing where this is going

---
+++ gigantron_generators/task/task_generator.rb
@@ -0,0 +1,50 @@
+class TaskGenerator < RubiGen::Base
+
+  default_options :author => nil
+
+  attr_reader :name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @name = args.shift
+    extract_options
+  end
+
+  def manifest
+    record do |m|
+      m.directory "tasks/"
+      m.template  "tasks/task.rake", "tasks/#{@name}.rake"
+      m.directory "test/"
+      m.directory "test/tasks/"
+      m.template  "test/tasks/test_task.rb", "test/tasks/test_#{@name}.rb"
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Creates a ...
+
+USAGE: #{$0} #{spec.name} name
+EOS
+    end
+
+    def add_options!(opts)
+      # opts.separator ''
+      # opts.separator 'Options:'
+      # For each option below, place the default
+      # at the top of the file next to "default_options"
+      # opts.on("-a", "--author=\"Your Name\"", String,
+      #         "Some comment about this option",
+      #         "Default: none") { |options[:author]| }
+      # opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+    end
+
+    def extract_options
+      # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
+      # Templates can access these value via the attr_reader-generated methods, but not the
+      # raw instance variable value.
+      # @author = options[:author]
+    end
+end
